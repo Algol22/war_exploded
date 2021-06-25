414cb3d4cea04eb9cc67ca8272d97c2f89b2de19
anfr <andriyslb@gmail.com>
2021-06-25 16:03:18 +0300

    level 1

---
+++ MVC/java/MVCPattern.java
@@ -0,0 +1,29 @@
+import controller.CourseController;
+import model.Course;
+import view.CourseView;
+
+public class MVCPattern {
+    public static void main(String[] args) {
+
+        Course model  = retriveCourseFromDatabase();
+
+        CourseView view = new CourseView();
+
+        CourseController controller = new CourseController(model, view);
+
+        controller.updateView();
+
+        controller.setCourseName("Python");
+        System.out.println("nAfter updating, Course Details are as follows");
+
+        controller.updateView();
+    }
+
+    private static Course retriveCourseFromDatabase(){
+        Course course = new Course();
+        course.setName("Java");
+        course.setId("01");
+        course.setCategory("Programming");
+        return course;
+    }
+}
